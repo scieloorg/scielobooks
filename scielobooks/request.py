a81631642f8c05562b3b6bd4e654e9de9f35959d
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2011-06-08 16:50:46 -0300

    renaming relational deb session name in request

--- scielobooks/request.py
+++ scielobooks/request.py
@@ -3,6 +3,6 @@ from pyramid.decorator import reify
 
 class MyRequest(Request):
     @reify
-    def rel_db(self):
+    def rel_db_session(self):
         maker = self.registry.settings['rel_db.sessionmaker']
         return maker()
