a2867c27125728fec425662e01ed921b63b13227
gustavofonseca <gustavofons@gmail.com>
2011-06-16 16:26:03 -0300

    moved the javascript to a separated file. applied some permissions to async functions

---
+++ scielobooks/static/js/panel.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $('.meetings').change(function(){
+        var selected = $('option:selected', this).val();
+        var evaluation = selected.split('_')[0];
+        var meeting = selected.split('_')[1];
+        
+        $('#meeting_load_icon_'+evaluation).show();
+
+        $.post("/staff/function/setmeeting/",
+               {"evaluation":evaluation, "meeting":meeting}, function(res){
+                   $('#meeting_load_icon_'+evaluation).hide();
+        });                    
+    });
+
+    $('.committee-decision').change(function(){
+        var selected = $('option:selected', this).val();                    
+        var evaluation = selected.split('_')[0];
+        var decision = selected.split('_')[1];
+        
+        $('#committee-decision_load_icon_'+evaluation).show();
+
+        $.post("/staff/function/setcommitteedecision/",
+               {"evaluation":evaluation, "decision":decision}, function(res){
+                   $('#committee-decision_load_icon_'+evaluation).hide();
+        });                                   
+    });
+    
+    $('.action_publish').click(function(){
+        var selected = $(this).attr('id');
+        var evaluation = selected.split('_')[0];
+
+        $('#actions_load_icon_'+evaluation).show();
+
+        $.post("/staff/function/actionpublish/",
+               {"evaluation":evaluation}, function(res){
+                   $('#actions_load_icon_'+evaluation).hide();
+                   location.reload();
+        });
+    });
+
+    $('.action_unpublish').click(function(){
+        var selected = $(this).attr('id');
+        var evaluation = selected.split('_')[0];
+
+        $('#actions_load_icon_'+evaluation).show();
+
+        $.post("/staff/function/actionunpublish/",
+               {"evaluation":evaluation}, function(res){
+                   $('#actions_load_icon_'+evaluation).hide();
+                   location.reload();
+        });
+    });
+});
