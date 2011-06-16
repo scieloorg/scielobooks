28d579ec156ad0fdda4fc825a9e13a438b1d363a
gustavofonseca <gustavofons@gmail.com>
2011-06-16 15:53:27 -0300

    removed some egg directories.

--- build/lib.linux-x86_64-2.6/scielobooks/resources.py
+++
@@ -1,7 +0,0 @@
-from pyramid.security import Allow, Everyone
-
-class Root(object):
-    __acl__ = [ (Allow, Everyone, 'view'),
-                (Allow, 'group:editors', 'list') ]
-    def __init__(self, request):
-        self.request = request
