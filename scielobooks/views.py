e9da61dbd3fa37094bd867caa7f7934136295da4
gustavofonseca <gustavofons@gmail.com>
2011-06-21 14:11:38 -0300

    added forbidden exception handling

--- scielobooks/views.py
+++ scielobooks/views.py
@@ -4,7 +4,10 @@ from pyramid.httpexceptions import HTTPInternalServerError, HTTPFound
 from pyramid.security import remember, forget
 from pyramid.url import route_url
 from pyramid.renderers import get_renderer
+from pyramid.i18n import TranslationStringFactory
+_ = TranslationStringFactory('scielobooks')
 
+import base64
 
 BASE_TEMPLATE = 'scielobooks:templates/base.pt'
 
@@ -22,3 +25,10 @@ def set_language(request):
 
 def attach(request):
     pass
+
+def custom_forbidden_view(request):
+    caller_url = base64.b64encode(request.url)
+    request.session.flash(_('You are not authorized to access this page. Try logging in before proceed.'))
+
+    return HTTPFound(location=request.route_path('users.login')+'?caller=%s' % caller_url)
+    
