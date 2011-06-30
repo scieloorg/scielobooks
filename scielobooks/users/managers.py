e78cec0e4b50c6de7b48347b0cd9e6c30323bdd2
gustavofonseca <gustavofons@gmail.com>
2011-06-30 15:14:40 -0300

    added forgot password feature

--- scielobooks/users/managers.py
+++ scielobooks/users/managers.py
@@ -3,8 +3,10 @@ from sqlalchemy.orm.exc import NoResultFound
 import models
 import transaction
 from datetime import datetime
+from Crypto.Hash import SHA256
 
 ACTIVATED = 'ACTIVATED'
+RECOVERED = 'RECOVERED'
 
 class InvalidActivationKey(Exception):
     def __init__(self, message=None):
@@ -20,8 +22,8 @@ class ActivationError(Exception):
     def __repr__(self):
         return '%s(%r)' % (self.__class__.__name__, self.message)
 
-class RegistrationProfileManager(object):
 
+class RegistrationProfileManager(object):
     @staticmethod
     def activate_user(activation_key, request):
         if activation_key == ACTIVATED:
@@ -49,16 +51,18 @@ class RegistrationProfileManager(object):
         return reg_profile.user
     
     @staticmethod
-    def send_activation_mail(user, request):
+    def send_activation_mail(user, request, message=None):
         if user.registration_profile.activation_key == ACTIVATED:
             raise InvalidActivationKey()
 
-        activation_url = request.route_url('users.activation') + '?key=%s' % user.registration_profile.activation_key
+        activation_url = request.route_url('users.activation') + '?key=%s' % user.registration_profile.activation_key        
+        if message is None:
+            message = "Hello %s, to activate your account please visit %s" % (user.username, activation_url)
 
         message = Message(subject="account activation",
                   sender=request.registry.settings['mail.default_sender'],
                   recipients=[user.email],
-                  body="Hello %s, to activate your account please visit %s" % (user.username, activation_url))
+                  body=message)
         request.registry['mailer'].send(message)
         transaction.commit()
 
@@ -70,3 +74,52 @@ class RegistrationProfileManager(object):
             pass
         
         return exclude_total
+
+
+class AccountRecoveryManager(object):
+    @staticmethod
+    def redefine_password(recovery_key, new_password, request):
+        if recovery_key == RECOVERED:
+            raise InvalidActivationKey()
+            
+        try:
+            account = request.rel_db_session.query(models.AccountRecovery).filter_by(recovery_key=recovery_key).one()
+        except NoResultFound:
+            raise InvalidActivationKey()
+
+        if datetime.now() > account.expiration_date:
+            raise InvalidActivationKey()
+
+        account.user.password = SHA256.new(new_password).hexdigest()
+        account.user.password_encryption = 'SHA256'
+        account.recovery_key = RECOVERED
+        account.recovery_date = datetime.now()
+
+        request.rel_db_session.add(account)
+        try:
+            request.rel_db_session.commit()
+        except IntegrityError:
+            request.rel_db_session.rollback()
+            raise ActivationError()
+        
+        return account.user
+
+    @staticmethod
+    def send_recovery_mail(user, request, message=None):
+        if user.account_recovery[-1].recovery_key == RECOVERED:
+            raise InvalidActivationKey()
+
+        recovery_url = request.route_url('users.recover_password') + '?key=%s' % user.account_recovery[-1].recovery_key
+        if message is None:
+            message = "Hello %s, to recover your account please visit %s" % (user.username, recovery_url)
+
+        message = Message(subject="account recovery",
+                  sender=request.registry.settings['mail.default_sender'],
+                  recipients=[user.email],
+                  body=message)
+        request.registry['mailer'].send(message)
+        transaction.commit()
+    
+    @staticmethod
+    def clean_expired(request):
+        pass
--- scielobooks/users/models.py
+++ scielobooks/users/models.py
@@ -69,4 +69,23 @@ class RegistrationProfile(Base):
         self.user = user
         self.activation_key = SHA256.new(user.username).hexdigest()
         self.expiration_date = datetime.now() + timedelta(1)
+
+
+class AccountRecovery(Base):
+    __tablename__ = 'account_recovery'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+
+    recovery_key = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    expiration_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    recovery_date = sqlalchemy.Column(sqlalchemy.DateTime)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('user.id'))
+    user = relationship("User", backref=backref('account_recovery', order_by=id,
+        uselist=True), cascade='all, delete, delete-orphan', single_parent=True)
+
+    def __init__(self, user):
+        self.user = user
+        self.recovery_key = SHA256.new(user.username+ str(datetime.now())).hexdigest()
+        self.expiration_date = datetime.now() + timedelta(1)
         
