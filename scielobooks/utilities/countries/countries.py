408aeb1874b2afe8d2e123a592daaa4d743193fc
gustavofonseca <gustavofons@gmail.com>
2011-11-11 03:18:38 +0800

    fixed ticket #130 Campo "country of publication"

--- scielobooks/utilities/countries/countries.py
+++ scielobooks/utilities/countries/countries.py
@@ -1,8 +1,11 @@
 # coding: utf-8
 import json
+import os
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+DATA_FILE = os.path.join(CURRENT_PATH, 'countries.json')
 
 class Countries(object):
-    __countries = json.load(open('countries.json'))
+    __countries = json.load(open(DATA_FILE))
 
     def __init__(self, language):
         self.__localized_countries = dict((code, country[language]) for code, country in self.__countries.items())
