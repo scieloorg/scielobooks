c48766a0ecf37ad13a0a3ccfbd0315348bea7aea
gustavofonseca <gustavofons@gmail.com>
2011-11-22 13:58:54 -0200

    fixed ticket #119 Ao clicar fora da janela de gestão dos dados de Partes de livros, a janela se fecha e os dados são perdidos

--- scielobooks/staff/forms.py
+++ scielobooks/staff/forms.py
@@ -352,10 +352,12 @@ class PartForm():
         base_schema['notes'].title = localizer.translate(_('Notes'))
         base_schema['notes'].description = localizer.translate(_('Notes'))
 
+        btn_cancel = deform.form.Button(name='btn_cancel', title=localizer.translate(_('Cancel')),
+                               type='submit', value='cancel', disabled=False)
         btn_submit = deform.form.Button(name='btn_submit', title=localizer.translate(_('Submit')),
                                type='submit', value='submit', disabled=False)
 
-        form = deform.Form(base_schema, buttons=(btn_submit,))
+        form = deform.Form(base_schema, buttons=(btn_cancel, btn_submit))
         functions.customize_form_css_class(form, **cls.widget_classes)
 
         return form
