fa8564848ca850dd9f7b97c839775e9422b48d73
Gustavo Fonseca <gustavofons@gmail.com>
2012-10-24 14:51:07 -0200

    Adicionados filtros de livros por letra inicial e por editora

--- scielobooks/api/views.py
+++ scielobooks/api/views.py
@@ -1,8 +1,13 @@
 # coding: utf-8
+import urllib
+
 import couchdbkit
 from pyramid import exceptions
 
 from scielobooks.staff import models
+
+
+APP_DOMAIN = 'http://books.scielo.org'
 
 
 def list_publishers(request):
@@ -11,7 +16,7 @@ def list_publishers(request):
         resp = {
             'total_items': data['value'],
             'title': data['key'],
-            '_id': data['key'],
+            '_id': urllib.quote(data['key']),
         }
 
         return resp
@@ -45,28 +50,30 @@ def list_alphasum(request):
 
 def list_books(request):
 
+    _couch_viewname = 'scielobooks/books_by_alpha'
+
     def _prepare_response(data):
         book = models.Monograph(**data)
 
         if getattr(book, 'pdf_file', None):
-            pdf_file_url = request.route_path(
+            pdf_file_url = APP_DOMAIN + request.route_path(
                 'catalog.pdf_file', sbid=book._id, part=book.shortname)
         else:
             pdf_file_url = ''
 
         if getattr(book, 'epub_file', None):
-            epub_file_url = request.route_path(
+            epub_file_url = APP_DOMAIN + request.route_path(
                 'catalog.epub_file', sbid=book._id, part=book.shortname)
         else:
             epub_file_url = ''
 
         if getattr(book, 'cover', None):
-            cover_url = request.route_path('catalog.cover', sbid=book._id)
+            cover_url = APP_DOMAIN + request.route_path('catalog.cover', sbid=book._id)
         else:
             cover_url = ''
 
         if getattr(book, 'cover_thumbnail', None):
-            cover_thumb_url = request.route_path('catalog.cover_thumbnail', sbid=book._id)
+            cover_thumb_url = APP_DOMAIN + request.route_path('catalog.cover_thumbnail', sbid=book._id)
         else:
             cover_thumb_url = ''
 
@@ -96,16 +103,23 @@ def list_books(request):
             'synopsis': data.get('synopsis', ''),
             'year': data.get('year', ''),
             'title': data.get('title', ''),
-            'creators': [{
-                'name': '',
-                'role': '',
-                'link_resume': '',
-            }],
+            'creators': book._creators_by_roles(),
         }
         return resp
 
+    kwargs = {}
+    filter_initial = request.params.get('filter_initial', None)
+    filter_publisher = request.params.get('filter_publisher', None)
+
+    if filter_initial:
+        kwargs['key'] = filter_initial.upper()
+
+    if filter_publisher:
+        kwargs['key'] = filter_publisher.upper()
+        _couch_viewname = 'scielobooks/books_by_publisher'
+
     try:
-        books = [_prepare_response(book['doc']) for book in request.db.view('scielobooks/books_by_alpha', include_docs=True)]
+        books = [_prepare_response(book['doc']) for book in request.db.view(_couch_viewname, include_docs=True, **kwargs)]
     except couchdbkit.ResourceNotFound:
         raise exceptions.NotFound()
 
