e36446d9fd1632d2bb304500227e88d5759ecb3b
Gustavo Fonseca <gustavo@gfonseca.net>
2014-03-28 17:17:30 -0300

    Adicionado o endpoint /book/{id}/

--- scielobooks/api/views.py
+++ scielobooks/api/views.py
@@ -10,6 +10,69 @@ from scielobooks.staff import models
 APP_DOMAIN = 'http://books.scielo.org'
 
 
+def _prepare_response(data, request):
+    """This is a function to help us to format a book as json
+    """
+    book = models.Monograph(**data)
+
+    if getattr(book, 'pdf_file', None):
+        pdf_file_url = APP_DOMAIN + request.route_path(
+            'catalog.pdf_file', sbid=book._id, part=book.shortname)
+    else:
+        pdf_file_url = ''
+
+    if getattr(book, 'epub_file', None):
+        epub_file_url = APP_DOMAIN + request.route_path(
+            'catalog.epub_file', sbid=book._id, part=book.shortname)
+    else:
+        epub_file_url = ''
+
+    if getattr(book, 'cover', None):
+        cover_url = APP_DOMAIN + request.route_path('catalog.cover', sbid=book._id)
+    else:
+        cover_url = ''
+
+    if getattr(book, 'cover_thumbnail', None):
+        cover_thumb_url = APP_DOMAIN + request.route_path('catalog.cover_thumbnail', sbid=book._id)
+    else:
+        cover_thumb_url = ''
+
+    resp = {
+        'publisher': data.get('publisher', ''),
+        '_id': data.get('_id', ''),
+        'pisbn': data.get('isbn', ''),
+        'eisbn': data.get('eisbn', ''),
+        'language': data.get('language', ''),
+        'updated': data.get('creation_date', ''),
+        'cover_thumbnail': {
+            'type': 'image/jpeg',
+            'uri': cover_thumb_url,
+        },
+        'cover': {
+            'type': 'image/jpeg',
+            'uri': cover_url,
+        },
+        'synopsis': data.get('synopsis', ''),
+        'year': data.get('year', ''),
+        'title': data.get('title', ''),
+        'creators': book._creators_by_roles(),
+    }
+
+    if pdf_file_url:
+        resp.update({'pdf_file': {
+            'type': 'application/pdf',
+            'uri': pdf_file_url,
+        }})
+
+    if epub_file_url:
+        resp.update({'epub_file': {
+            'type': 'application/epub+zip',
+            'uri': epub_file_url,
+        }})
+
+    return resp
+
+
 def list_publishers(request):
 
     def _prepare_response(data):
@@ -52,66 +115,6 @@ def list_books(request):
 
     _couch_viewname = 'scielobooks/books_by_alpha'
 
-    def _prepare_response(data):
-        book = models.Monograph(**data)
-
-        if getattr(book, 'pdf_file', None):
-            pdf_file_url = APP_DOMAIN + request.route_path(
-                'catalog.pdf_file', sbid=book._id, part=book.shortname)
-        else:
-            pdf_file_url = ''
-
-        if getattr(book, 'epub_file', None):
-            epub_file_url = APP_DOMAIN + request.route_path(
-                'catalog.epub_file', sbid=book._id, part=book.shortname)
-        else:
-            epub_file_url = ''
-
-        if getattr(book, 'cover', None):
-            cover_url = APP_DOMAIN + request.route_path('catalog.cover', sbid=book._id)
-        else:
-            cover_url = ''
-
-        if getattr(book, 'cover_thumbnail', None):
-            cover_thumb_url = APP_DOMAIN + request.route_path('catalog.cover_thumbnail', sbid=book._id)
-        else:
-            cover_thumb_url = ''
-
-        resp = {
-            'publisher': data.get('publisher', ''),
-            '_id': data.get('_id', ''),
-            'pisbn': data.get('isbn', ''),
-            'eisbn': data.get('eisbn', ''),
-            'language': data.get('language', ''),
-            'updated': data.get('creation_date', ''),
-            'cover_thumbnail': {
-                'type': 'image/jpeg',
-                'uri': cover_thumb_url,
-            },
-            'cover': {
-                'type': 'image/jpeg',
-                'uri': cover_url,
-            },
-            'synopsis': data.get('synopsis', ''),
-            'year': data.get('year', ''),
-            'title': data.get('title', ''),
-            'creators': book._creators_by_roles(),
-        }
-
-        if pdf_file_url:
-            resp.update({'pdf_file': {
-                'type': 'application/pdf',
-                'uri': pdf_file_url,
-            }})
-
-        if epub_file_url:
-            resp.update({'epub_file': {
-                'type': 'application/epub+zip',
-                'uri': epub_file_url,
-            }})
-
-        return resp
-
     kwargs = {}
     filter_initial = request.params.get('filter_initial', None)
     filter_publisher = request.params.get('filter_publisher', None)
@@ -124,7 +127,7 @@ def list_books(request):
         _couch_viewname = 'scielobooks/books_by_publisher'
 
     try:
-        books = [_prepare_response(book['doc']) for book in request.db.view(_couch_viewname, include_docs=True, **kwargs)]
+        books = [_prepare_response(book['doc'], request) for book in request.db.view(_couch_viewname, include_docs=True, **kwargs)]
     except couchdbkit.ResourceNotFound:
         raise exceptions.NotFound()
 
@@ -134,4 +137,9 @@ def list_books(request):
 def list_changes(request):
     consumer = couchdbkit.Consumer(request.db)
     return consumer.fetch(since=request.params.get('since', 0))
+
+
+def show_book(request):
+    book = request.db.get(request.matchdict['id'])
+    return _prepare_response(book, request)
 
